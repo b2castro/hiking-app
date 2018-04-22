89ea4e328f7ed910f31f765a139486e1709268b5
ksbains <k1bains@ucsd.edu>
2018-04-22 14:05:20 -0700

    changed the homepage

--- public/js/hikeAPI.js
+++ public/js/hikeAPI.js
@@ -1,22 +1,8 @@
-<html>
-<head>
-	<title>Test Page</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/index.css">
-</head>
-<body>
-	<h1>Hey This is the HikingAPP</h1>
-	<button onclick="displayTrailInfo()">Trails</button>
-	<div id="trails-view"> </div>
-</body>
-<!-- <script type="text/javascript" src="../hikeAPI.js"></script> -->
-<script type="text/javascript">
-	function displayTrailInfo() {
+function displayTrailInfo(lati, longi) {
 
         //var trail = $(this).attr("data-name");
-        let lat = 32.856385;
-        let long = -117.202936;
+        let lat = lati;
+        let long = longi;
         let maxDistance = 10;
         //https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200252138-f373bf62d2f3d526e56392c58af99811                                                                                                 
         var queryURL = 'https://www.hikingproject.com/data/get-trails?' + 'lat=' + lat + '&lon=' + long + '&maxDistance=' + maxDistance + '&key=200252138-f373bf62d2f3d526e56392c58af99811';
@@ -34,7 +20,8 @@
 
             // Storing the rating data
             let name = response.trails[i].name;
-            let stars = response.trails[i].stars;
+            let stars = Math.floor(response.trails[i].stars);
+            console.log("the star scroe is ", stars);
             let starVotes = response.trails[i].starVotes;
             
             let difficulty = response.trails[i].difficulty;
@@ -43,9 +30,19 @@
             // Creating an element to have the rating displayed
             var title = $("<h1>").text(name); 
             var h2One = $("<h2>").text("Star Rating: " + stars);
+            
+            // for (var i = 0; i < stars; i++) {
+            //     h2One.text(h2One.text() + '\u2605');
+            // }
+
             var h2Two = $("<h2>").text("Star Votes: " + starVotes);
             var h2Three = $("<h2>").text("Difficulty: " + difficulty);
-            var imgURL = response.trails[i].imgMedium;
+            var imgURL;
+            if(response.trails[i].imgMedium){
+                imgURL = response.trails[i].imgMedium;
+            }else{
+                imgURL = "images/trailSearch.jpg";
+            }
 
             // Creating an element to hold the image
             var image = $("<img>").attr("src", imgURL);
@@ -64,16 +61,14 @@
           };//end of for loop. 
           
    //        {{#each trails}}
-			// 	<div class="trail col-xs-4 col-sm-4 col-md-4 col-lg-4 card" id="{{id}}">
-			// 		<h1 class="Title">{{name}}</h1>
-			// 		<p>Star Rating: + {{stars}} </p>
-			// 		<p>Star Votes: + {{starVotes}} </p>
-			// 		<p>Difficulty: + {{difficulty}} </p>
-			// 		<img src="{{imgMedium}}" class="img">
-			// 	</div>
-			// {{/each}}
+            //  <div class="trail col-xs-4 col-sm-4 col-md-4 col-lg-4 card" id="{{id}}">
+            //      <h1 class="Title">{{name}}</h1>
+            //      <p>Star Rating: + {{stars}} </p>
+            //      <p>Star Votes: + {{starVotes}} </p>
+            //      <p>Difficulty: + {{difficulty}} </p>
+            //      <img src="{{imgMedium}}" class="img">
+            //  </div>
+            // {{/each}}
 
         });// end of done function 
       }// end of displayTrailInfo function 
-</script>
-</html>
